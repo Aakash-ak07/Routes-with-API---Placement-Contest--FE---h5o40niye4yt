92eee96b4368bf6ab5d9e73ecf308cb042dc834e
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-04-09 16:12:14 +0530

    commit-contest

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,28 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function Top() {
-    const [cryptoArr, setCryptoArr] = useState([]);
+  const [cryptoArr, setCryptoArr] = useState([]);
 
-    const fetchData =()=>{
-        fetch("http://api.coinlore.net/api/tickers/").then(response =>{
-            return response.json();
-        })
-    }
+  const url = "https://api.coinlore.net/api/tickers/";
 
-    return (
-        <div className='top-container'>
-            <h1>Top 10 Global Crypto Coins</h1>
-            <div className='display-container'>
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCryptoArr(data.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-                <div className='coin-container'>
-                    <p className='rank-para'>Rank #rank</p>
-                    <h3 className='coin-symbol'>name (symbol)</h3>
-                    <p className='price-para'>Price: price_usd</p>
-                </div>
-
-            </div>
-        </div>
-    )
+  return (
+    <div className="top-container">
+      <h1>Top 10 Global Crypto Coins</h1>
+      <div className="display-container">
+        {cryptoArr &&
+          cryptoArr
+            .filter((item) => {
+              return item.rank <= 10;
+            })
+            .map((item, idx) => (
+              <div key={idx} className="coin-container">
+                <p className="rank-para">Rank # {item.rank}</p>
+                <h3 className="coin-symbol">
+                  {item.name} ({item.symbol})
+                </h3>
+                <p className="price-para">Price: {item.price_usd}</p>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
 }
 
-export default Top
+export default Top;
